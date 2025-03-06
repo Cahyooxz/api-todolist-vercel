72e97b41ce6bb0d71a8a525a8d04432ee473da50
Cahyooxz <cahyorasyid011627@gmail.com>
2025-03-06 10:32:16 +0700

    web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TodolistController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,8 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
 return view('welcome');
 });
-Route::get('/tes', function () {
-  return response()->json([
-      'status' => 'success'
-    ]);
+Route::prefix('todolist')->group(function(){
+  Route::get('/',[TodolistController::class,'index'])->name(name: 'todolist.index');
+  Route::post('/save',[TodolistController::class,'store'])->name('todolist.store');
+  Route::get('/edit/{id}',[TodolistController::class,'edit'])->name('todolist.edit');
+  Route::put('/update/{id}',[TodolistController::class,'update'])->name('todolist.update');
+  Route::put('/status/{id}',[TodolistController::class,'set_status'])->name('todolist.set_status');
+  Route::delete('/delete/{id}',[TodolistController::class,'destroy'])->name('todolist.destroy');
 });
+
